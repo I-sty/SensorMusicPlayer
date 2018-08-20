747052c12db3e6d50e01ce7c7bea34f7fef6d2e5
szollosi.istv4n@gmail.com <NemTudom2018>
2018-08-20 22:31:42 +0300

    Added Project Lombok

--- app/src/main/java/com/kalosis/sensormusicplayer/Counter.java
+++ app/src/main/java/com/kalosis/sensormusicplayer/Counter.java
@@ -1,14 +1,11 @@
 package com.kalosis.sensormusicplayer;
 
+@lombok.Data
 public class Counter {
   private long counter;
 
   Counter() {
     this.counter = 0;
-  }
-
-  public synchronized long getCounter() {
-    return counter;
   }
 
   public synchronized void increment() {
